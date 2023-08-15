1b47007dbd195ebcf9c1634996e72cce611473d1
Liquorhands <breyerkevin2019@gmail.com>
2023-08-16 01:42:14 +0200

    1

---
+++ moonshine/consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0x2f3883abdbb28410ae9c75dbb98cfd96f44945e8";
+export const tokenContractAddress =
+  "0xfA2B3C7F03D890F4761281E144825bdEbbCfbfc4";
+export const stakingContractAddress =
+  "0x860E710cE67c72B9972E824dd55B2A761ca8641a";
